a0d446048badadb2976e80ad76e41adfa9f22c84
Daniel Warner <danywarner@gmail.com>
2015-12-15 16:52:29 -0500

    Initial Commit

---
+++ Info Drugs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Info Drugs
+//
+//  Created by Daniel Warner on 12/15/15.
+//  Copyright © 2015 Daniel Warner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
